2337fa8b0da19c8734da57bedb0339a3925e3abe
eexy <eexdhe@gmail.com>
2025-07-12 11:55:49 +0200

    feat: add treesiter object

--- lua/eexy/plugins/telescope.lua
+++ lua/eexy/plugins/telescope.lua
@@ -53,7 +53,8 @@ return {
 
 
         vim.keymap.set('n', "<leader>fb", builtin.buffers, {})
-        vim.keymap.set('n', "<leader>fy", builtin.lsp_workspace_symbols, {})
+        vim.keymap.set('n', "<leader>fy", builtin.lsp_document_symbols, {})
+        vim.keymap.set('n', "<leader>fwy", builtin.lsp_workspace_symbols, {})
         vim.keymap.set('n', "<leader>fm", builtin.marks)
 
         -- open input and search with telescope
