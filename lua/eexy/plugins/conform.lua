60d1ceebc50b80b89ed52ecbeb03a7818cbb71ee
eexy <eexdhe@gmail.com>
2025-11-09 15:36:11 +0100

    feat: add rust lsp and custom telescope mappings to delete buffer

--- lua/eexy/plugins/conform.lua
+++ lua/eexy/plugins/conform.lua
@@ -4,10 +4,11 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-                javascript = { "prettier", "biome", stop_after_first = true },
-                typescript = { "prettier", "biome", stop_after_first = true },
-                vue = { "prettier", "biome", stop_after_first = true },
-                markdown = { "prettier", "biome", stop_after_first = true },
+                javascript = { "prettierd", "prettier", "biome", stop_after_first = true },
+                json = { "prettierd", "prettier", "biome", stop_after_first = true },
+                typescript = { "prettierd", "prettier", "biome", stop_after_first = true },
+                vue = { "prettierd", "prettier", "biome", stop_after_first = true },
+                markdown = { "prettierd", "prettier", "biome", stop_after_first = true },
             },
             format_on_save = {
                 -- These options will be passed to conform.format()
--- lua/eexy/plugins/telescope.lua
+++ lua/eexy/plugins/telescope.lua
@@ -4,6 +4,18 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
+        local actions = require('telescope.actions')
+
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    n = {
+                        ['dd'] = actions.delete_buffer
+                    }
+                }
+            }
+        })
+
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope live grep' })
