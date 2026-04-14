95c7023df195c50de8d4e6c28d7fd496cbf8e71c
eexy <eexdhe@gmail.com>
2026-04-14 19:18:44 +0200

    refactor: put keymaps for oil in oil plugin config file

--- lua/eexy/keymap.lua
+++ lua/eexy/keymap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", "<cmd>:Oil<Cr>")
 
 vim.keymap.set("n", "<leader>w", "<cmd>:w<CR>")
 vim.keymap.set("n", "<C-a>h", "<C-w>h")
@@ -8,5 +7,3 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "gd", "<C-]>")
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
-vim.keymap.set("n", "<S-l>", "<cmd>:bnext<CR>")
-vim.keymap.set("n", "<S-h>", "<cmd>:bprevious<CR>")
--- lua/eexy/plugins/oil.lua
+++ lua/eexy/plugins/oil.lua
@@ -3,5 +3,6 @@ return {
     lazy = false,
     config = function()
         require("oil").setup()
+        vim.keymap.set("n", "<leader>pv", "<cmd>:Oil<Cr>")
     end
 }
